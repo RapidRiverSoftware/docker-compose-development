fbae1e61ec91a63cd48e7de966d0effaa7a7bacd
opheus2 <ominiabohsdavid@gmail.com>
2022-02-09 17:32:52 +0100

    setup backend src

---
+++ backend/routes/api.v1.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('quote/random', function () {
+    $quote = Http::get("https://zenquotes.io/api/random");
+    return response()->json(["quote" => $quote->object()[0]->h]);
+});
